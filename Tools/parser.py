c90b69c7bc49b2ccab829d8c484eb4c196b95b8f
Jackson Xie <jx82593@gmail.com>
2019-09-19 02:03:36 -0400

    Finished json creation tool

--- Tools/parser.py
+++ Tools/parser.py
@@ -4,13 +4,27 @@ As it stands, the champions.json file is absolutely  horrid to parse through
 This tool will create my own personal json file for my program to use
 """
 import requests
+import json
 
-def request():
+def data():
+    ddragon_url = "https://ddragon.leagueoflegends.com/cdn/9.18.1/data/en_US/champion.json"
+    ddragon_json = requests.get(ddragon_url).json()['data']
 
+    champions = {}
+
+    for i in ddragon_json.values():
+        champions.update({i['key']:i['id']})
+
+    return champions
 
 
 
 def main():
+    champions = data()
+
+    with open('champion.json', 'w', encoding='utf-8') as f:
+        json.dump(champions, f, ensure_ascii=False, indent=4)
+
     return 0
 
 
